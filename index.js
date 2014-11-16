2b8e11bcb96dbcaf4c45c978198b089e3416adb9
NukesForKids <matt@capit.me>
2014-11-16 09:16:19 -0800

    Add some index stuff.

--- index.js
+++ index.js
@@ -1,14 +1,18 @@
 var express = require('express');
 var app = express();
-var pg = require('pg');
+//var pg = require('pg');
 
 app.set('port', (process.env.PORT || 5000));
-app.use(express.static(__dirname+ '/public'));
+app.use(express.static(__dirname + '/public'));
 
-app.get('/', function(request, response){
-	response.send(cool());
+app.get(function (req, res){
+    console.log("REQ is "+req);
 });
 
-app.listen(app.get('port'), function (){
+app.get('/', function(request, response){
+	response.send("Hello world!");
+});
+
+var server = app.listen(app.get('port'), function (){
 	console.log("Capit Node.js is running on localhost:" + app.get('port'));
 });
